22076b8c9ccf6759a3343c327811cfcef975d218
Simen <lsimen89@gmail.com>
2021-01-16 17:41:04 +0100

    Update routes

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/test', (req, res) => {
+    rjson = {
+        'navn': 'Simen'
+    }
+    res.json(rjson) 
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,18 +1,14 @@
+'use strict';
 const express = require('express');
 
 const path = require('path');
 
+const indexRouter = require("../routes/index.js");
+
 const app = express();
 
-app.get('/test', (req, res) => {
-    rjson = {
-        'navn': 'Simen'
-    }
-    res.json(rjson)   
-})
+app.get('/test', indexRouter); 
 
 const port = process.env.PORT || 8080;
 
 app.listen(port);
-
-console.log('App lytter');
